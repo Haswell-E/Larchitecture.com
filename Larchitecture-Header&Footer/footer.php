f7fe0632420524a35a78292f49d992e35b551d94
NYA Zakaria <zakaria.nya@hetic.net>
2017-05-17 22:39:00 +0200

    Header & Footer

---
+++ Larchitecture-Header&Footer/footer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zakar
+ * Date: 17/05/2017
+ * Time: 20:26
+ */
+
+?>
+
+
+<footer class="col-lg-12 col-xs-12">
+    <div class="col-lg-8 col-lg-offset-2 footer">
+        <div class="col-lg-4">
+            <h3>Newsletter</h3>
+            <h5>Inscription à la newsletter</h5>
+            <form>
+                <input type="email" name="" class="col-lg-8 email" placeholder="votre adresse e-mail">
+                <input type="submit" name="" class="col-lg-4 submit">
+            </form>
+        </div>
+        <div class="col-lg-2 col-lg-offset-2">
+            <ul class="list-unstyled">
+                <li>Accueil</li>
+                <li>Nos Revues</li>
+                <li>Commandes</li>
+            </ul>
+        </div>
+        <div class="col-lg-2 col-lg-offset-2">
+            <ul class="list-unstyled">
+                <li>Actualité</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    </div>
+    <div class="col-lg-8 col-lg-offset-2 footer-rights">
+        <hr>
+        <a class="text-center col-lg-12 col-xs-12">Mention Légales</a>
+        <P class="text-center col-lg-12 col-xs-12">2017 - copyright © Lachitecture.com Tous droits resérvés.</P>
+    </div>
+</footer>
+
+</body>
+</html>
---
+++ Larchitecture-Header&Footer/header.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zakar
+ * Date: 17/05/2017
+ * Time: 20:26
+ */
+
+?>
+
+
+<header>
+    <nav class="navbar">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#"><img src="img/img-layout/logo_larchitecture.com.png"></a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Nos Revues</a></li>
+                <li><a href="#">Commandes</a></li>
+                <li><a href="#">Actualités</a></li>
+                <li><a href="#">Connexion</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
